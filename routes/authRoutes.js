893f8cd5039add361cddc9fa49b1bd329ec1ff6e
Alex Lee <alexytlee@gmail.com>
2019-06-09 10:59:47 -0700

    Set up store

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -8,11 +8,17 @@ module.exports = app => {
 		})
 	);
 
-	app.get('/auth/google/callback', passport.authenticate('google'));
+	app.get(
+		'/auth/google/callback',
+		passport.authenticate('google'),
+		(req, res) => {
+			res.redirect('/surveys');
+		}
+	);
 
 	app.get('/api/logout', (req, res) => {
 		req.logout();
-		res.send(req.user); // Empty response because we are no longer signed in
+		res.redirect('/');
 	});
 
 	app.get('/api/current_user', (req, res) => {
